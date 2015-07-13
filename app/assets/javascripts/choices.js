15278071b83271c06917a3a196a160faaf30ddee
Drew Finstrom & Chris Sefkin <student@epicodus.com>
2015-07-13 16:56:42 -0700

    fix issue causing page to reset to top on click

--- app/assets/javascripts/choices.js
+++ app/assets/javascripts/choices.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
   console.log("choices.js");
-  $(".thumbnail").click(function() {
+  $(".thumbnail").click(function(e) {
     var voteforID = $(this).attr("id");
     var choiceID = $(this).attr("data-id");
     var voteInputID = $("#edit_choice_" + choiceID + " > #choice_option_" + voteforID + "_votes");
     voteInputID.val("1");
     var form = voteInputID.parent('form');
+    e.preventDefault();
     form.submit();
+    return false;
   });
 });
