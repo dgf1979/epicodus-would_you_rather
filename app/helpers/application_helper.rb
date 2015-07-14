b9f1534652c71fae86c6e401cff6ad1d886c40ae
Drew Finstrom & Chris Sefkin <student@epicodus.com>
2015-07-14 15:01:30 -0700

    style comments; add date time format helpers; fix bug with setting comment text fields val to 0 after voting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def human_date(artime)
+    t = artime.to_time
+    return t.localtime.strftime("%b. %d")
+  end
+
+  def human_time(artime)
+    t = artime.to_time
+    return t.localtime.strftime("%I:%M %p")
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :choice
   validates :text, presence: true
   validates :choice_id, presence: true
+  default_scope { order('created_at DESC') }
 end
